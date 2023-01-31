b00320c1e518b5c183920816292b59c52873411f
dlrudco <kyungchae123@gmail.com>
2023-01-31 22:57:13 +0900

    sdg

--- backend/kale/tests/assets/kfp_dsl/simple_data_passing.py
+++ backend/kale/tests/assets/kfp_dsl/simple_data_passing.py
@@ -196,29 +196,7 @@ if __name__ == "__main__":
 
     # Get or create an experiment and submit a pipeline run
     import kfp
-    import requests
-
-    HOST = "http://143.248.152.217:33391"  
-    USERNAME = "user@example.com"
-    PASSWORD = "12341234"
-    NAMESPACE = "kubeflow" 
-
-    session = requests.Session()
-    response = session.get(HOST)
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-
-    data = {"login": USERNAME, "password": PASSWORD}
-    session.post(response.url, headers=headers, data=data)
-    session_cookie = session.cookies.get_dict()["authservice_session"]
-
-    client = kfp.Client(
-        host=f"{HOST}/pipeline",
-        cookies=f"authservice_session={session_cookie}",
-        namespace=NAMESPACE,
-    )
+    client = kfp.Client()
     experiment = client.create_experiment('test')
 
     # Submit a pipeline run
